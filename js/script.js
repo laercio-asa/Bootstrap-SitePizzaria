97e5c3a49573a9b09c641f2b244ce080f3c025e8
Laercio Azevedo de Sá <laercio.asa@senacsp.edu.br>
2025-08-14 20:04:13 -0300

    14/08/2025

--- js/script.js
+++ js/script.js
@@ -1,13 +1,16 @@
 window.addEventListener('scroll', function () {
     const posicao = window.scrollY;
-    console.log(posicao);
+    //console.log(posicao);
     if (posicao > 100) {
         //document.getElementById("navbar").classList.add("bg-body-tertiary");
-        alfa = 1 - ((posicao>=300)?0:(posicao - 100) / 200);
+        alfa = ((posicao>=300)?1:(posicao - 100) / 200);
         console.log(alfa);
-        this.document.getElementById("navbar").style.backgroundColor = "rgba(248, 249, 250, " + alfa + ")";
+        this.document.getElementById("navbar").style.backgroundColor = 
+            "rgba(248, 249, 250, " + alfa + ")";
+            
     }
     else {
-        this.document.getElementById("navbar").style.backgroundColor = "rgba(248, 249, 250, 0)";
+        this.document.getElementById("navbar").style.backgroundColor = 
+            "rgba(248, 249, 250, 0)";
     }
 });
